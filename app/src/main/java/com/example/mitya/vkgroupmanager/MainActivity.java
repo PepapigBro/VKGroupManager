bdae30d4d5b2b865f06f35d9850753ebec8ad81d
PepapigBro <offsuit@mail.ru>
2019-02-10 12:47:06 +0300

    Initial

---
+++ app/src/main/java/com/example/mitya/vkgroupmanager/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mitya.vkgroupmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
